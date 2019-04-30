62630d0426112a9cd011abe61be65a32a76c3b2c
Dongkwan Kim <todoaskit@gmail.com>
2019-05-01 02:22:44 +0900

    Add UnitType

--- SFN/enums.py
+++ SFN/enums.py
@@ -4,3 +4,11 @@ import enum
 class LabelType(enum.Enum):
     ONE_LABELS_TO_ALL_TASK = 0
     ONE_LABEL_TO_ONE_CLASS = 1
+
+
+class UnitType(enum.Enum):
+    FILTER = 0
+    NEURON = 1
+
+    def __str__(self):
+        return self.name
